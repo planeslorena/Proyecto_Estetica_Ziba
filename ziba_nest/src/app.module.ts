d89e4e39581ec64eb6e45f6a565ade8800e19e26
planeslorena <planeslorena@gmail.com>
2024-05-09 20:07:35 -0300

    creacion login controller y service

--- ziba_nest/src/app.module.ts
+++ ziba_nest/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [],
+  imports: [JwtModule.register({
+    secret:
+      'hjfdsalhfdsahfjkdsakreaurceukfbukalsfyuej43243545y47988367+++fdsfjhdsifyhujdshfjkdsahfjkdskgfhjdsgfygsuyejkgfhdjgfsgejfgdjhsdgfhjsekfyhdbsyfjegfjdysgfjyefgydegfhjseyrfeyr63254342343',
+    signOptions: { expiresIn: '1h' },
+  }),],
+  
   controllers: [AppController],
   providers: [AppService],
 })
