9f0cff6334335a34848e71d80d8ed2a82beaabf2
Guada-Chojo <gchojohernandez@gmail.com>
2024-05-11 17:52:17 -0300

    nav links funcionales

--- ziba-react/src/app/(pages)/login/page.tsx
+++ ziba-react/src/app/(pages)/login/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 import { Logo } from "@/app/components/logo/logo";
-import { PortadaHome } from "@/app/components/bannerHome/portadaHome";
-import { Menu } from "@/app/components/nav/nav";
 
 export default function Login() {
     return (
@@ -9,13 +7,11 @@ export default function Login() {
           <header>
             <div>
               <Logo></Logo>
-              <Menu></Menu>
             </div>
     
           </header>
     
           <main>
-           <PortadaHome></PortadaHome>
           </main>
           
         </>
