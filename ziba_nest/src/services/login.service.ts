b9aff579b361bfce9eb4ef43eae43306485dada5
planeslorena <planeslorena@gmail.com>
2024-05-11 14:50:07 -0300

    avance login

--- ziba_nest/src/services/login.service.ts
+++ ziba_nest/src/services/login.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
+import { RowDataPacket } from 'mysql2';
+import { DatabaseService } from './db.service';
+import loginQueries from './queries/login.queries';
 
 @Injectable()
 export class LoginService {
   salt: string = '$2a$08$W59jWcwio1TiLx4A8iRyTO';
   joseHash: string;
-  constructor(private jwtService: JwtService) {
+  constructor(private jwtService: JwtService, private dbService: DatabaseService) {
     this.genSalt();
   }
 
@@ -16,6 +19,17 @@ export class LoginService {
 
   async validateUser(username: string, password: string): Promise<any> {
     //obtener de la base de datos el usuario
+    const resultQuery: RowDataPacket[] = await this.dbService.executeSelect(
+      loginQueries.selectUsuario,
+      [username],
+    );
+
+    if (resultQuery.length == 0) {
+      throw new HttpException('El usuario no existe', HttpStatus.FORBIDDEN)
+    }
+
+    console.log(resultQuery);
+
     if (username === 'jose') {
       const passEncriptado = await bcrypt.hash(password, this.salt);
       if (this.joseHash == passEncriptado) {
