b5dff9244c91a76dea8264d1c60b0ce18f624065
Guada-Chojo <gchojohernandez@gmail.com>
2024-05-28 15:40:40 -0300

    inicio de filtro lista de turnos

--- ziba-react/src/app/components/appointmentList/appointmentList.tsx
+++ ziba-react/src/app/components/appointmentList/appointmentList.tsx
@@ -1,53 +1,71 @@
 import React from 'react';
 import './appointmentList.css'
-import { Card, Table } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 const list = [{
-  id: 'a',
-  firstname: 'Robin',
-  lastname: 'Wieruch',
-  year: 1988,
+  profesion: 'depiladora',
+  nombre: 'Romina Benegas',
+  servicio: 'Depilación brasileña',
+  dia: '27/05/2024',
+  horario: '14:00',
 },
 {
-  id: 'b',
-  firstname: 'Dave',
-  lastname: 'Davidds',
-  year: 1990,
+  profesion: 'cosmetóloga',
+  nombre: 'Marisa Ruiz',
+  servicio: 'Peeling',
+  dia: '27/05/2024',
+  horario: '15:00',
 },
 {
-  id: 'b',
-  firstname: 'Dave',
-  lastname: 'Davidds',
-  year: 1990,
+  profesion: 'cosmetóloga',
+  nombre: 'Marisa Ruiz',
+  servicio: 'Limpieza Facial',
+  dia: '27/05/2024',
+  horario: '16:00',
 },
 {
-  id: 'b',
-  firstname: 'Dave',
-  lastname: 'Davidds',
-  year: 1990,
+  profesion: 'masajista',
+  nombre: 'Naomi Almeida',
+  servicio: 'Masaje cuerpo entero',
+  dia: '27/05/2024',
+  horario: '17:00',
 },
 {
-  id: 'b',
-  firstname: 'Dave',
-  lastname: 'Davidds',
-  year: 1990,
+  profesion: 'manicura',
+  nombre: 'Maiten Suarez',
+  servicio: 'Esculpidas',
+  dia: '27/05/2024',
+  horario: '18:00',
 }];
 
 export const AppointmentList = () => {
   return (
-    <div id="div1" className='div1'>
-      <div id="div2" className='div2'>
-        {list.map((item: any) => {
+    <div className='appointment-list-container'>
+      <div className='scroller-container'>
+        <h4 className='scroller-title'>MIS TURNOS</h4>
+        {list.map((item: any, index: any) => {
           return (
-            <Card id="div3" className='div3'>
-              <Card.Img variant="top" src="holder.js/100px180" />
+            <div key={index} className='appointment-cards-container'>
+            <Card className='appointment-cards'>
+              <div className='card-container'>
+              <img className='img-appointment-card' src={`imagenes/professionals/${item.profesion}.png`} />
               <Card.Body>
-                <Card.Title>{item.firstname}</Card.Title>
+                <Card.Title>{item.nombre}</Card.Title>
                 <Card.Text>
-                  {item.lastname}
+                  Servicio: {item.servicio}
                 </Card.Text>
+                <div className='card-container'>
+                <Card.Text>
+                  Día: {item.dia}
+                </Card.Text>
+                <Card.Text>
+                  Hora: {item.horario}
+                </Card.Text>
+                </div>
               </Card.Body>
+              </div>
             </Card>
+            </div>
           )
         })}
       </div>
