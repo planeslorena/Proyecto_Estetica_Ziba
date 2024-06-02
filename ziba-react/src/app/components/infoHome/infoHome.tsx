7c0452a2c2ba221781390e0549a7fd1b20658cb8
AyelenPorqueres <ayelen.porqueres04@gmail.com>
2024-06-02 20:29:42 -0300

    text-infohome

--- ziba-react/src/app/components/infoHome/infoHome.tsx
+++ ziba-react/src/app/components/infoHome/infoHome.tsx
@@ -11,7 +11,7 @@ export const InfoHome = () => {
             <div className='square-info-home d-flex justify-content-center align-items-center'>
                 <div className='border-square'>
                     <div className='border-square-two'>
-                        <div className='square-two-info'> <p className='title-info-home'> ¿Por qué elegir Zibá?</p> <p className="text-info-home">En el centro de estética integral Zibá, ubicado en la ciudad de Olvarría, ofrecemos los mejores servicios con capacitación constante y la última tecnología para que te renueves por dentro y por fuera. Nuestra atención es personalizada, siendo la seguridad y privacidad de cada paciente pilares de nuestro trabajo. La meta principal es ofrecer un tratamiento acorde a las necesidades de cada paciente; para realzar la belleza, sin perder la naturalidad. </p></div>
+                        <div className='square-two-info'> <p className='title-info-home'> ¿Por qué elegir Zibá?</p> <p className="text-info-home">En el centro de estética integral Zibá, ubicado en la ciudad de Olavarría, ofrecemos los mejores servicios con capacitación constante y la última tecnología para que te renueves por dentro y por fuera. Nuestra atención es personalizada, siendo la seguridad y privacidad de cada paciente pilares de nuestro trabajo. La meta principal es ofrecer un tratamiento acorde a las necesidades de cada paciente; para realzar la belleza, sin perder la naturalidad. </p></div>
                     </div>
 
                 </div>
