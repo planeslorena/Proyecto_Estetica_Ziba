283c8dbc47ef036e5192fdc3a26fe227e1fc8edb
Guada-Chojo <gchojohernandez@gmail.com>
2024-06-30 21:43:27 -0300

    seguimiento page servicios

--- ziba-react/src/app/(pages)/appointment/page.tsx
+++ ziba-react/src/app/(pages)/appointment/page.tsx
@@ -1,23 +1,53 @@
 'use client'
 import { withRoles } from "@/app/components/HOC/whitRoles";
 import { CardService } from "@/app/components/cardService/cardService";
+import CustomPagination from "@/app/components/cardService/pagination";
 import { Menu } from "@/app/components/nav/nav";
+import { useState } from "react";
+
+const specialities = [
+  { img: 'cosmetologia', prof: 'Marisa Ruiz', price: '10000', services: ['Peeling', 'Limpieza facial'] },
+  { img: 'depilacion', prof: 'Romina Benegas', price: '10000', services: ['Bozo', 'Piernas'] },
+  { img: 'manicuria', prof: 'Maiten Suarez', price: '10000', services: ['Soft gel', 'Semipermanente'] },
+  { img: 'maquillaje', prof: 'Eva Jimenez', price: '10000', services: ['Novias', 'Quinceañeras'] },
+  { img: 'masoterapia', prof: 'Naomi Almeida', price: '10000', services: ['Masaje terapéutico', 'Masaje lifático'] },
+  { img: 'peluqueria', prof: 'Irene Acosta', price: '10000', services: ['Corte', 'Nutrición'] },
+]
 
 function AppointmentPage() {
+  const [currentPage, setCurrentPage] = useState(1); // set the current page
+  const pageSize = 2; // show row in table
+
+  const paginatedData = specialities.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
   return (
     <>
       <header>
         <div>
           <Menu></Menu>
-
         </div>
-
       </header>
 
       <main>
-        <div className="d-flex justify-content-center">
-          <CardService></CardService>
-        </div>
+        {paginatedData.map((specialities: any) => (
+          <div className="d-flex flex-column align-items-center">
+
+            <>
+              <CardService speciality={specialities}></CardService>
+
+            </>
+
+          </div>
+        ))}
+        <CustomPagination
+          itemsCount={specialities.length}
+          itemsPerPage={pageSize}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          alwaysShown={true}
+        />
       </main>
     </>
   )
--- ziba-react/src/app/components/cardService/cardService.tsx
+++ ziba-react/src/app/components/cardService/cardService.tsx
@@ -4,8 +4,10 @@ import 'react-calendar/dist/Calendar.css';
 import './cardService.css'
 import { SubmitHandler, useForm, Controller } from "react-hook-form";
 import { useEffect, useState } from "react";
+import { DescriptionModal } from "./descriptionModal";
 
 interface data {
+    speciality: string,
     service: string,
     day: string,
     hour: string,
@@ -25,7 +27,7 @@ const schedules: Schedule[] = [
     { day: 'Saturday', times: ['09:00 AM', '10:00 AM', '11:00 AM'] },
 ];
 
-const speciality = [
+const specialities = [
     { img: 'cosmetologia', prof: 'Marisa Ruiz', price: '10000', services: ['Peeling', 'Limpieza facial'] },
     { img: 'depilacion', prof: 'Romina Benegas', price: '10000', services: ['Bozo', 'Piernas'] },
     { img: 'manicuria', prof: 'Maiten Suarez', price: '10000', services: ['Soft gel', 'Semipermanente'] },
@@ -34,7 +36,12 @@ const speciality = [
     { img: 'peluqueria', prof: 'Irene Acosta', price: '10000', services: ['Corte', 'Nutrición'] },
 ]
 
-export const CardService = () => {
+interface cardServiceProps {
+    speciality: any,
+}
+
+export const CardService: React.FC<cardServiceProps> = ({ speciality }) => {
+    const [show, setShow] = useState<any>(false);
     const [value, setValue] = useState<any>(new Date());
     const [availableTimes, setAvailableTimes] = useState<string[]>([]);
     const [format, setFormat] = useState<string>('');
@@ -42,6 +49,11 @@ export const CardService = () => {
     const onSubmit: SubmitHandler<data> = (data) => {
         console.log(data);
     }
+
+    const handleClose = () => {
+        setShow(false);
+    }
+
     const now = new Date();
     const tomorrow = new Date(now);
     tomorrow.setDate(now.getDate() + 1);
@@ -77,97 +89,106 @@ export const CardService = () => {
     }, [value]);
 
     return (
-        <div /* className="d-flex flex-column align-items-evenly" */>
-            {speciality.map((speciality) => (
-                <Card className="d-flex flex-row conteiner-card-service">
-                    <img src={`imagenes/services/${speciality.img}.jpg`} alt="" />
-                    <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column justify-content-evenly">
-                        <div>
-                            <Card.Title className="title-primary d-flex justify-content-evenly align-items-center">
-                                <p className="p-title p-service">{speciality.img.toUpperCase()}</p>
-                                <p className="p-title">Prof.{speciality.prof}</p>
-                            </Card.Title>
+        <div className="d-flex flex-wrap">
+            <Card className="d-flex flex-row conteiner-card-service">
+                <img src={`imagenes/services/${speciality.img}.jpg`} alt="" />
+                <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column justify-content-evenly align-items-evenly">
+                    <div>
+                        <Card.Title className="title-primary d-flex justify-content-evenly align-items-center">
+                            <p className="p-title p-service">{speciality.img.toUpperCase()}</p>
+                            <p className="p-title">Prof.{speciality.prof}</p>                        
+                        </Card.Title> 
                             <Card.Title className="d-flex justify-content-evenly title-secondary align-items-center">
+                                <>
                                 <select
-                                    {...register('service')}>
+                                    {...register('service', {
+                                        required: 'Por favor elija un servicio'
+                                    })}>
                                     <option value="" selected disabled hidden>Servicio</option>
-                                    {speciality.services.map((service) => (
+                                    {speciality.services.map((service: any) => ( 
                                         <option value={service}>{service}</option>
-                                    ))}
+                                        ))}
                                 </select>
-                                <p className="p-title">¿Que es?</p>
-                            </Card.Title>
-                        </div>
-                        <div className="d-flex">
-                            <Card.Body className="d-flex justify-content-evenly">
-                                <div className="d-flex flex-column align-items-center">
-                                    <p>Reserve aquí su turno</p>
-                                    <Controller
-                                        name="day"
-                                        control={control}
-                                        rules={{ required: 'Por favor elija un día' }}
-                                        render={({ field }) => (
-                                            <Calendar
-                                                onChange={(date) => {
-                                                    setValue(date);
-                                                    const options: Intl.DateTimeFormatOptions = {
-                                                        year: 'numeric',
-                                                        month: 'numeric',
-                                                        day: 'numeric',
-                                                    };
-                                                    field.onChange(value.toLocaleDateString(undefined, options));
-                                                }}
-                                                value={value}
-                                                minDetail='month'
-                                                maxDetail="month"
-                                                minDate={startDate}
-                                                maxDate={endDate}
-                                                tileDisabled={({ date }) => isDateDisabled(date)}
-                                                view="month"
-                                                prev2Label={null}
-                                                next2Label={null}
-                                                showNeighboringMonth={false}
-                                                locale='es-419'
-                                                defaultValue={tomorrow}
-                                            />
-                                        )}
+                                <small>{errors.service?.message}</small>
+                                <p onClick={() => setShow(true)} className="p-title">¿Que es?</p>
+                                <DescriptionModal service={speciality.services[0]} desc='lorem ipsum' show={show} handleClose={handleClose}></DescriptionModal> 
+                               </> 
+                            
+                        </Card.Title> 
+                    </div>
+                    <Card.Body className="d-flex justify-content-around">
+                        <div className="d-flex flex-column align-items-center">
+                            <p>Reserve aquí su turno</p>
+                            <Controller
+                                name="day"
+                                control={control}
+                                rules={{ required: 'Por favor elija un día' }}
+                                render={({ field }) => (
+                                    <Calendar
+                                        onChange={(date) => {
+                                            setValue(date);
+                                            const options: Intl.DateTimeFormatOptions = {
+                                                year: 'numeric',
+                                                month: 'numeric',
+                                                day: 'numeric',
+                                            };
+                                            field.onChange(value.toLocaleDateString(undefined, options));
+                                        }}
+                                        value={value}
+                                        minDetail='month'
+                                        maxDetail="month"
+                                        minDate={startDate}
+                                        maxDate={endDate}
+                                        tileDisabled={({ date }) => isDateDisabled(date)}
+                                        view="month"
+                                        prev2Label={null}
+                                        next2Label={null}
+                                        showNeighboringMonth={false}
+                                        locale='es-419'
+                                        defaultValue={tomorrow}
                                     />
-                                    <small>{errors.day?.message}</small>
-                                </div>
-                                <div className="d-flex flex-column justify-content-evenly">
-                                    <div className="container-price-service">
-                                        <p className="p-input-service">Precio</p>
-                                        <input type="text" defaultValue={speciality.price} disabled className="input-precio inputs-service" />
-                                    </div>
-                                    <div className="container-day-service">
-                                        <p className="p-input-service">Día</p>
-                                        <input type="text" defaultValue={format} disabled className="inputs-service" />
-                                    </div>
-                                    <div className="container-hour-service">
-                                        <p className="p-input-service" >Hora</p>
-                                        <select className="container-option-hour inputs-service"
-                                            {...register("hour", {
-                                                required: "Por favor ingrese una hora",
-                                            })}>
-                                            {availableTimes.length > 0 ? (
-                                                availableTimes.map((time, index) => (
-                                                    <option key={index} value={time}>
-                                                        {time}
-                                                    </option>
-                                                ))
-                                            ) : (
-                                                <option>No hay horarios disponibles</option>
-                                            )}
-                                        </select>
-                                        <small>{errors.hour?.message}</small>
-                                    </div>
-                                    <button type="submit" disabled={!isValid} className="button-reservar-service">Reservar</button>
-                                </div>
-                            </Card.Body>
+                                )}
+                            />
+                            <small>{errors.day?.message}</small>
                         </div>
-                    </form>
-                </Card>
-            ))}
+                        <div className="d-flex flex-column justify-content-evenly">
+                            <input type="text" defaultValue={speciality.img} hidden
+                                {...register('speciality')} />
+                            <div className="container-price-service">
+                                <p className="p-input-service">Precio</p>
+                                <input type="text" defaultValue={speciality.price} disabled className="input-precio inputs-service" />
+                            </div>
+                            <div className="container-day-service">
+                                <p className="p-input-service">Día</p>
+                                <input type="text" defaultValue={format} disabled className="inputs-service" />
+                            </div>
+                            <div className="container-hour-service">
+                                <p className="p-input-service" >Hora</p>
+                                <select className="container-option-hour inputs-service"
+                                    {...register("hour", {
+                                        required: "Por favor ingrese una hora",
+                                    })}>
+                                    {availableTimes.length > 0 ? (
+                                        <>
+                                            <option value="" selected disabled>Elija un horario</option>
+                                            {availableTimes.map((time, index) => (
+                                                <option key={index} value={time}>
+                                                    {time}
+                                                </option>
+                                            ))}
+                                        </>
+                                    ) : (
+                                        <option value="" selected disabled>No hay horarios disponibles</option>
+                                    )}
+                                </select>
+                                <small>{errors.hour?.message}</small>
+                            </div>
+                            <button type="submit" disabled={!isValid} className="button-reservar-service">Reservar</button>
+                        </div>
+                    </Card.Body>
+                </form>
+            </Card>
+
         </div>
 
 
